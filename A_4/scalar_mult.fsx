e0c8f4cc5f4845644c8c3cc8162b550bfa447fa9
Franz <franz.zbinden7@gmail.com>
2025-11-09 15:56:03 -0400

    Added recursive & iter functions to scale a list to the given number

---
+++ A_4/scalar_mult.fsx
@@ -0,0 +1,17 @@
+// This file determines the scalar multiplication of an integer and a list of integers using different methods:
+
+// Recursive function to scale a list to the given number
+let rec scalarMult num lst = 
+    match lst with 
+    | [] -> []
+    | hd::tl -> (hd * num) :: scalarMult num tl
+
+// Function iter to scale a list to the given number
+let scalarMultIter num lst =
+
+    let rec loop num lst =
+        match lst with 
+        | [] -> []
+        | hd::tl -> (hd * num) :: loop num tl
+
+    loop num lst

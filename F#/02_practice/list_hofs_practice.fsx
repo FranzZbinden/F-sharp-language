2aebb59b2c2058baa0ffd875c5e1edc6f6b8e75a
Franz <franz.zbinden7@gmail.com>
2025-11-23 17:20:36 -0400

    Added practice funcions using filter, map, fold.

--- F#/02_practice/list_hofs_practice.fsx
+++ F#/02_practice/list_hofs_practice.fsx
@@ -13,3 +13,32 @@ let foldReverse lst =
 // filter the odd numbers from a list
 let filterOdds lst = 
     List.filter (fun curr -> curr % 2 = 0) lst
+
+
+let mapDouble lst = 
+    List.map (fun curr -> curr*2 ) lst
+
+
+// Return a list of factorials from 1 to n
+// (using List.fold or your own factorial function).
+let factorialRange n = 
+    List.fold (fun acc curr -> acc * curr) 1 [1..n]
+
+
+let pipeEvenSquares lst = 
+    lst
+    |> List.filter (fun curr -> curr % 2 = 0)
+    |> List.map (fun curr -> curr * curr)
+
+
+// Description:
+// Use List.fold to multiply all elements.
+let foldProduct lst = List.fold (fun acc curr -> curr * acc) 1 lst
+
+
+// Add 1 to every element using List.map.
+let mapIncrement lst = List.map(fun curr -> curr + 1) lst
+
+
+
+
